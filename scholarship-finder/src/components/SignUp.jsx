05fa99b4559602956f4abbb81d784c2cb50efff7
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-01-30 10:24:38 +0530

    Error sign up fixed

--- scholarship-finder/src/components/SignUp.jsx
+++ scholarship-finder/src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import './Signup.css';
+import './SignUp.css';
 import { useNavigate } from 'react-router-dom';
 const Signup = () => {
     const navigate = useNavigate();
